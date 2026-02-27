2977f084ba4251ba6a8a6440ed5a7a8d1bb02b70
philip.yomi@gmail.com <philip.yomi@gmail.com>
2026-02-27 08:31:02 -0500

    update tavily search

--- main.py
+++ main.py
@@ -9,28 +9,31 @@ async def main():
 
     while True:
         # get inputs
+        job_link = input("JOB LINK: ")
         target_company = input("TARGET COMPANY: ")
         current_company = input("CURRENT COMPANY: ")
-        currently_employed = input("CURRENTLY EMPLOYED: ")
-        current_role = input("CURRENT ROLE: ")
-        current_job_tenure = input("CURRENT JOB TENURE: ")
-        risk_tolerance = int(input("RISK TOLERANCE: "))
-        career_stage = input("CAREER STAGE: ")
-        career_priority = input("CAREER PRIORITY: ")
+        # currently_employed = input("CURRENTLY EMPLOYED: ")
+        # current_role = input("CURRENT ROLE: ")
+        # current_job_tenure = input("CURRENT JOB TENURE: ")
+        # risk_tolerance = int(input("RISK TOLERANCE: "))
+        # career_stage = input("CAREER STAGE: ")
+        # career_priority = input("CAREER PRIORITY: ")
 
         # run agent
         research = await my_agent.run(
+            job_link=job_link,
             target_company=target_company,
             current_company=current_company,
-            currently_employed=currently_employed,
-            current_role=current_role,
-            current_job_tenure=current_job_tenure,
-            risk_tolerance=risk_tolerance,
-            career_stage=career_stage,
-            career_priority=career_priority,
+            # currently_employed=currently_employed,
+            # current_role=current_role,
+            # current_job_tenure=current_job_tenure,
+            # risk_tolerance=risk_tolerance,
+            # career_stage=career_stage,
+            # career_priority=career_priority,
         )
 
-        print(research["industry_research"])
+        # print(research["industry_research"])
+        print(research["target_company_profile"])
 
 
 if __name__ == "__main__":
