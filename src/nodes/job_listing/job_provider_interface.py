e883aba0a10a681e5ae3e001060d9eb0b2450c27
philip.yomi@gmail.com <philip.yomi@gmail.com>
2026-03-27 16:41:19 -0400

    changed annotation

--- src/nodes/job_listing/job_provider_interface.py
+++ src/nodes/job_listing/job_provider_interface.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
+from typing import Any
 
 
 class JobProviderInterface(ABC):
     @abstractmethod
-    def fetch_jobs(self):
+    def fetch_jobs(self) -> Any:  # temporary annotation
         pass
