8950d6f6df251f76cd5781110557fe6d211fb95c
philip.yomi@gmail.com <philip.yomi@gmail.com>
2026-03-29 18:27:38 -0400

    refactor job search and node chaining

--- src/nodes/job_listing/theirstack.py
+++ src/nodes/job_listing/theirstack.py
@@ -29,12 +29,12 @@ class TheirStack(job_provider_interface.JobProviderInterface):
         self.preferred_location = [
             "CA",
         ]
-        self.job_seniority = [
-            "staff",
-            "senior",
-            "mid-level",
-        ]
-        self.minimum_salary = 120000
+        # self.job_seniority = [
+        #     "staff",
+        #     "senior",
+        #     "mid-level",
+        # ]
+        # self.minimum_salary = 120000
         self.preferred_job_board = ["linkedin.com"]
         self.preferred_employment_status = [
             "full_time",
@@ -46,10 +46,10 @@ class TheirStack(job_provider_interface.JobProviderInterface):
     def fetch_jobs(self):
         payload = {
             "page": 0,  # research how to use pagination
-            "limit": 25,  # theirstack max limit
+            "limit": 1,  # theirstack max limit < -- currently experimental
             "job_title_or": self.preferred_jobs,
             "job_country_code_or": self.preferred_location,
-            "posted_at_max_age_days": 7,
+            "posted_at_max_age_days": 3,
             # "job_seniority_or": self.job_seniority,
             # "min_salary_usd": self.minimum_salary,
             "url_domain_or": self.preferred_job_board,
@@ -73,71 +73,6 @@ class TheirStack(job_provider_interface.JobProviderInterface):
             return response.json()
         except Exception as e:
             print(f"Encountered error during 'fetch_jobs' operation. Error: {e}")
-
-    def normalize_jobs(self, *raw_jobs):
-
-        normalized_jobs = []
-
-        for job in raw_jobs:
-            desired_job_fields = {}
-
-            desired_job_fields["job_title"] = job.get("job_title", "")
-            desired_job_fields["job_posting_link"] = job.get("url", "")
-            desired_job_fields["company"] = job.get("company", "")
-            desired_job_fields["job_city"] = job.get("location", "")
-            desired_job_fields["job_state"] = job.get("state_code", "")
-            desired_job_fields["country"] = job.get("country", "")
-            desired_job_fields["salary"] = job.get("salary_string", "")
-            desired_job_fields["minimum_salary"] = job.get("min_annual_salary_usd", "")
-            desired_job_fields["company_domain"] = job.get("company_domain", "")
-            desired_job_fields["hiring_team"] = job.get("hiring_team", [])
-            desired_job_fields["employment_statuses"] = job.get(
-                "employment_statuses", []
-            )
-            desired_job_fields["job_description"] = job.get("description", "")
-            desired_job_fields["company_name"] = job.get("company_object", {}).get(
-                "name", ""
-            )
-            desired_job_fields["company_industry"] = job.get("company_object", {}).get(
-                "industry", ""
-            )
-            desired_job_fields["company_employee_count"] = job.get(
-                "company_object", {}
-            ).get("employee_count", "")
-            desired_job_fields["company_num_jobs"] = job.get("company_object", {}).get(
-                "num_jobs", ""
-            )
-            desired_job_fields["company_url"] = job.get("company_object", {}).get(
-                "url", ""
-            )
-            desired_job_fields["company_linkedin_url"] = job.get(
-                "company_object", {}
-            ).get("linkedin_url", "")
-            desired_job_fields["is_recruiting_agency"] = job.get(
-                "company_object", {}
-            ).get("is_recruiting_agency", "")
-            desired_job_fields["company_founded_year"] = job.get(
-                "company_object", {}
-            ).get("founded_year", "")
-            desired_job_fields["publicly_traded_symbol"] = job.get(
-                "company_object", {}
-            ).get("publicly_traded_symbol", "")
-            desired_job_fields["yc_batch"] = job.get("company_object", {}).get(
-                "yc_batch", ""
-            )
-            desired_job_fields["total_funding_usd"] = job.get("company_object", {}).get(
-                "total_funding_usd", ""
-            )
-            desired_job_fields["last_funding_round_date"] = job.get(
-                "company_object", {}
-            ).get("last_funding_round_date", "")
-            desired_job_fields["last_funding_round_amount_readable"] = job.get(
-                "company_object", {}
-            ).get("last_funding_round_amount_readable", "")
-
-            normalized_jobs.append(desired_job_fields)
-
-            return normalized_jobs
 
 
 theirstack_provider = TheirStack()
--- src/nodes/job_posting_researcher.py
+++ src/nodes/job_posting_researcher.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from typing import cast
 
 from langchain_core.runnables import RunnableConfig
 
@@ -7,7 +8,8 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 root_dir = os.path.abspath(os.path.join(current_dir, "../../"))
 sys.path.append(root_dir)
 
-from src.models import TargetJobDetails  # noqa: E402
+# from src.models import TargetJobDetails  # noqa: E402
+from src.models import JobPostingModel  # noqa: E402
 from src.nodes.job_listing.theirstack import theirstack_provider  # noqa: E402
 from src.state import State  # noqa: E402
 
@@ -20,6 +22,72 @@ class JobPostingResearch:
         return {"raw_research": result}
 
     async def normalize_job(self, state: State, config: RunnableConfig):
-        # normalize jobs
-        normalized_jobs = theirstack_provider.normalize_jobs()
-        return {"job_posting_details": normalized_jobs}
+        list_of_jobs = []
+        raw_jobs = state["raw_research"].get("job_posting_raw")
+
+        if not raw_jobs:
+            raise ValueError("No jobs found, halting further processing")
+
+        for job in raw_jobs:
+            extracted_jobs_fields = {}
+
+            extracted_jobs_fields["job_title"] = job.get("job_title", "")
+            extracted_jobs_fields["job_posting_link"] = job.get("url", "")
+            extracted_jobs_fields["company"] = job.get("company", "")
+            extracted_jobs_fields["job_city"] = job.get("location", "")
+            extracted_jobs_fields["job_state"] = job.get("state_code", "")
+            extracted_jobs_fields["country"] = job.get("country", "")
+            extracted_jobs_fields["salary"] = job.get("salary_string", "")
+            extracted_jobs_fields["minimum_salary"] = job.get(
+                "min_annual_salary_usd", ""
+            )
+            extracted_jobs_fields["company_domain"] = job.get("company_domain", "")
+            extracted_jobs_fields["hiring_team"] = job.get("hiring_team", [])
+            extracted_jobs_fields["employment_statuses"] = job.get(
+                "employment_statuses", []
+            )
+            extracted_jobs_fields["job_description"] = job.get("description", "")
+            extracted_jobs_fields["company_name"] = job.get("company_object", {}).get(
+                "name", ""
+            )
+            extracted_jobs_fields["company_industry"] = job.get(
+                "company_object", {}
+            ).get("industry", "")
+            extracted_jobs_fields["company_employee_count"] = job.get(
+                "company_object", {}
+            ).get("employee_count", "")
+            extracted_jobs_fields["company_num_jobs"] = job.get(
+                "company_object", {}
+            ).get("num_jobs", "")
+            extracted_jobs_fields["company_url"] = job.get("company_object", {}).get(
+                "url", ""
+            )
+            extracted_jobs_fields["company_linkedin_url"] = job.get(
+                "company_object", {}
+            ).get("linkedin_url", "")
+            extracted_jobs_fields["is_recruiting_agency"] = job.get(
+                "company_object", {}
+            ).get("is_recruiting_agency", "")
+            extracted_jobs_fields["company_founded_year"] = job.get(
+                "company_object", {}
+            ).get("founded_year", "")
+            extracted_jobs_fields["publicly_traded_symbol"] = job.get(
+                "company_object", {}
+            ).get("publicly_traded_symbol", "")
+            extracted_jobs_fields["yc_batch"] = job.get("company_object", {}).get(
+                "yc_batch", ""
+            )
+            extracted_jobs_fields["total_funding_usd"] = job.get(
+                "company_object", {}
+            ).get("total_funding_usd", "")
+            extracted_jobs_fields["last_funding_round_date"] = job.get(
+                "company_object", {}
+            ).get("last_funding_round_date", "")
+            extracted_jobs_fields["last_funding_round_amount_readable"] = job.get(
+                "company_object", {}
+            ).get("last_funding_round_amount_readable", "")
+
+            job_data = cast(JobPostingModel, extracted_jobs_fields)
+            list_of_jobs.append(job_data)
+
+            return {"normalized_jobs": list_of_jobs}
